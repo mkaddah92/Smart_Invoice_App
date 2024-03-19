8a0d5839cf98a97926724d9d3bd83c2fb2b4cb0b
Mudar Kadah <mudar.kadah@sap.com>
2024-03-19 18:44:43 +0000

    with upload controller

--- app/Smart_Invoice_App.SmartInvoiceApp/webapp/ext/controller/ListReportExt.controller.js
+++ app/Smart_Invoice_App.SmartInvoiceApp/webapp/ext/controller/ListReportExt.controller.js
@@ -6,6 +6,28 @@ sap.ui.define([
 	'use strict';
 
 	return ControllerExtension.extend('SmartInvoiceApp.SmartInvoiceApp.ext.controller.ListReportExt', {
+		handleUploadComplete: function(oEvent) {
+			var sResponse = oEvent.getParameter("response"),
+				aRegexResult = /\d{4}/.exec(sResponse),
+				iHttpStatusCode = aRegexResult && parseInt(aRegexResult[0]),
+				sMessage;
+
+			if (sResponse) {
+				sMessage = iHttpStatusCode === 200 ? sResponse + " (Upload Success)" : sResponse + " (Upload Error)";
+				MessageToast.show(sMessage);
+			}
+		},
+
+		handleUploadPress: function() {
+			var oFileUploader = this.byId("fileUploader");
+			oFileUploader.checkFileReadable().then(function() {
+				oFileUploader.upload();
+			}, function(error) {
+				MessageToast.show("The file cannot be read. It may have changed.");
+			}).then(function() {
+				oFileUploader.clear();
+			});
+		},
 		onCreateInvoice: function(oEvent) {
 
             const that = this;
